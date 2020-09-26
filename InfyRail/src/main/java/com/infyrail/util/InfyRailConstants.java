4a765201b1352c43e28bb741560e454cb92fa5e9
User <User@DESKTOP-C13PCB4>
2020-09-26 23:03:48 +0530

    Added exception message for general and creating routes

--- InfyRail/src/main/java/com/infyrail/util/InfyRailConstants.java
+++ InfyRail/src/main/java/com/infyrail/util/InfyRailConstants.java
@@ -1,5 +1,19 @@
 package com.infyrail.util;
 
 public enum InfyRailConstants {
+	ROUTE_NOT_FOUND("route.not.found"),
+	GENERAL_EXCEPTION_MESSAGE("general.exception");
+	
+	private final String type;
+	
+	private InfyRailConstants(String type) {
+		this.type=type;
+	}
+	
+	@Override
+	public String toString() {
+		return this.type;
+	}
+	
 
 }
