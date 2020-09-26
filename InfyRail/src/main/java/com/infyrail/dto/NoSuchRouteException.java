ada0796d4454087f5759d0b64d899e330b8ad763
User <User@DESKTOP-C13PCB4>
2020-09-26 22:50:06 +0530

    Created custom no routes exception

---
+++ InfyRail/src/main/java/com/infyrail/dto/NoSuchRouteException.java
@@ -0,0 +1,17 @@
+package com.infyrail.dto;
+
+public class NoSuchRouteException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NoSuchRouteException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
