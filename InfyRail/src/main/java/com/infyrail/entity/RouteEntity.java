fe06b66f58a63c72f2075c6963d06861c433a675
User <User@DESKTOP-C13PCB4>
2020-09-26 22:52:22 +0530

    Function to convert Entity to DTO

--- InfyRail/src/main/java/com/infyrail/entity/RouteEntity.java
+++ InfyRail/src/main/java/com/infyrail/entity/RouteEntity.java
@@ -1,5 +1,6 @@
 package com.infyrail.entity;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -7,9 +8,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.infyrail.dto.RouteDTO;
+import com.infyrail.dto.TrainDTO;
 
 @Entity
+@Table(name = "routes")
 public class RouteEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,6 +24,7 @@ public class RouteEntity {
 	private String source;
 	private String destination;
 	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "route_id")
 	private List<TrainEntity> trainList;
 	public Integer getId() {
 		return id;
@@ -51,6 +59,16 @@ public class RouteEntity {
 		this.source = source;
 		this.destination = destination;
 		this.trainList = trainList;
+	}
+	
+	public RouteDTO convertToRouteDTO() {
+		List<TrainDTO> trainList = new LinkedList<TrainDTO>();
+		this.trainList.forEach(c->{
+			TrainDTO t = new TrainDTO(c.getId(),c.getTrainName(),c.getArrivalTime(),c.getDepartureTime(),c.getFare());
+			trainList.add(t);
+		});		
+		return new RouteDTO(this.id,this.destination,this.source,trainList);
+		
 	}
 	
 	
