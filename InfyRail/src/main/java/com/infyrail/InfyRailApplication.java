b451c48accb97f06f9241c49bfbb198a1becf5d8
User <User@DESKTOP-C13PCB4>
2020-09-27 13:59:09 +0530

    Property source annotation added

--- InfyRail/src/main/java/com/infyrail/InfyRailApplication.java
+++ InfyRail/src/main/java/com/infyrail/InfyRailApplication.java
@@ -2,8 +2,10 @@ package com.infyrail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:ValidationMessages.properties")
 public class InfyRailApplication {
 
 	public static void main(String[] args) {
