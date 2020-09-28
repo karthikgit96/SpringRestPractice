3cad01e8825fb8cd4757dadcf79f54c4fdeecfae
User <User@DESKTOP-C13PCB4>
2020-09-28 11:33:42 +0530

    Method to create route

--- InfyRail/src/main/java/com/infyrail/service/RouteService.java
+++ InfyRail/src/main/java/com/infyrail/service/RouteService.java
@@ -1,5 +1,6 @@
 package com.infyrail.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +17,9 @@ import com.infyrail.util.InfyRailConstants;
 @Transactional
 public class RouteService {
 	@Autowired
-	private RouteRepository routeRepository;
+	private RouteRepository routeRepository;//Instance of route repository to gain access to DAO
 	
+	//The below method is used to get route details from the repository
 	public RouteDTO getRouteDetails(Integer id) throws NoSuchRouteException{
 		Optional<RouteEntity> optional = routeRepository.findById(id);
 		RouteEntity routeEntity = optional.orElseThrow(()->new NoSuchRouteException(InfyRailConstants.ROUTE_NOT_FOUND.toString()));
@@ -25,5 +27,17 @@ public class RouteService {
 		return routeDTO;		
 	}
 	
+	//Method to create route
+	public Integer createRoute(RouteDTO routeDTO) throws Exception{
+		Integer retVal = null;
+		RouteEntity route = routeDTO.convertToEntity();
+		List<RouteEntity> existingList = routeRepository.findAll();
+		if(existingList.contains(route)) throw new Exception(InfyRailConstants.ROUTE_ALREADY_PRESENT.toString());
+		else {
+			routeRepository.saveAndFlush(route);
+			retVal = route.getId();
+		}
+		return retVal;	
+	}
 
 }
