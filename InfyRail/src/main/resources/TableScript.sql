2bdae81838720e79db351fe7451f67c1fc143bca
User <User@DESKTOP-C13PCB4>
2020-09-28 13:04:47 +0530

    Created new table train on route to implement many to many and inserted values to it

--- InfyRail/src/main/resources/TableScript.sql
+++ InfyRail/src/main/resources/TableScript.sql
@@ -12,14 +12,22 @@ create table trains(
 	train_name varchar(30),
 	arrival_time varchar(20),
 	departure_time varchar(20),
-	fare double(7,2),
-	route_id int, 
-	FOREIGN KEY (route_id) REFERENCES routes(id)
+	fare double(7,2)
 );
 
+create table trains_on_routes(
+	route_id int,
+	train_id bigint,
+	constraint foreign key(route_id) references routes(id),
+	constraint foreign key(train_id) references trains(id)
+);
 insert into routes(id,source,destination) values(100,'Bangalore','Mangalore');
-insert into trains(id,train_name,arrival_time,departure_time,fare,route_id) values(1000,'Karwar Express','08:30:00 AM','08:55:00 PM',1200,100);
-insert into trains(id,train_name,arrival_time,departure_time,fare,route_id) values(1001,'Kannur Express','08:30:00 AM','08:55:00 PM',1200,100);
+insert into trains(id,train_name,arrival_time,departure_time,fare) values(1000,'Karwar Express','08:30:00 AM','08:55:00 PM',1200);
+insert into trains(id,train_name,arrival_time,departure_time,fare) values(1001,'Kannur Express','08:30:00 AM','08:55:00 PM',1200);
+insert into trains_on_routes(route_id,train_id) values(100,1000);
+insert into trains_on_routes(route_id,train_id) values(100,1001);
+
 select * from routes;
 select * from trains;
+select * from trains_on_routes;
 
