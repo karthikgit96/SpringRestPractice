6c8a3540a15043aa87d82dde5858136fc96627a0
User <User@DESKTOP-C13PCB4>
2020-09-28 11:52:17 +0530

    create Route method implementation

--- InfyRail/src/main/java/com/infyrail/controller/RouteController.java
+++ InfyRail/src/main/java/com/infyrail/controller/RouteController.java
@@ -1,13 +1,17 @@
 package com.infyrail.controller;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,10 +26,20 @@ public class RouteController {
 	@Autowired
 	private RouteService routeService;
 	
+	@Autowired
+	private Environment env;
+	
 	@GetMapping(value = "{routeId}")
 	public ResponseEntity<RouteDTO> getRouteDetails(@PathVariable("routeId") @Pattern(regexp = "[0-9]{3}",message = "{route.routeId.invalid}") Integer id) throws NoSuchRouteException{
 		RouteDTO routeDTO = routeService.getRouteDetails(id);
 		return new ResponseEntity<RouteDTO>(routeDTO,HttpStatus.OK);		
+	}
+	
+	@PostMapping
+	public ResponseEntity<String> createRoute(@Valid @RequestBody RouteDTO routeDTO) throws Exception{
+		Integer id = routeService.createRoute(routeDTO);
+		String message = env.getProperty("route.create.success")+id.toString();
+		return new ResponseEntity<String>(message,HttpStatus.CREATED);		
 	}
 
 }
