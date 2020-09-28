e5bcf4662af64bb9c1113abf1d7e6866481a6d0e
User <User@DESKTOP-C13PCB4>
2020-09-28 11:33:27 +0530

    Adding hashcode and equals function to check equality based on source and destination

--- InfyRail/src/main/java/com/infyrail/entity/RouteEntity.java
+++ InfyRail/src/main/java/com/infyrail/entity/RouteEntity.java
@@ -70,6 +70,37 @@ public class RouteEntity {
 		return new RouteDTO(this.id,this.destination,this.source,trainList);
 		
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((destination == null) ? 0 : destination.hashCode());
+		result = prime * result + ((source == null) ? 0 : source.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RouteEntity other = (RouteEntity) obj;
+		if (destination == null) {
+			if (other.destination != null)
+				return false;
+		} else if (!destination.equals(other.destination))
+			return false;
+		if (source == null) {
+			if (other.source != null)
+				return false;
+		} else if (!source.equals(other.source))
+			return false;
+		return true;
+	}
+	
+	
 	
 	
 
