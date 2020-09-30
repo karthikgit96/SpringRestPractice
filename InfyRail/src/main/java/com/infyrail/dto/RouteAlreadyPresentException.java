39b815fa1430d4586bfd2f0856950aa3bce70736
User <User@DESKTOP-C13PCB4>
2020-09-30 10:40:14 +0530

    Added new Exception class to display message to show routes already added

---
+++ InfyRail/src/main/java/com/infyrail/dto/RouteAlreadyPresentException.java
@@ -0,0 +1,17 @@
+package com.infyrail.dto;
+
+public class RouteAlreadyPresentException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public RouteAlreadyPresentException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
