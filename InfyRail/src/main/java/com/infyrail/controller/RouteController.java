58d64f3812d7b60e66e38486e53bd2d2d5b9ed54
User <User@DESKTOP-C13PCB4>
2020-09-30 12:39:10 +0530

    getTrains method implemented to fetch train details based on source and destination

--- InfyRail/src/main/java/com/infyrail/controller/RouteController.java
+++ InfyRail/src/main/java/com/infyrail/controller/RouteController.java
@@ -1,5 +1,7 @@
 package com.infyrail.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 import javax.validation.constraints.Pattern;
 
@@ -13,10 +15,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.infyrail.dto.NoSuchRouteException;
 import com.infyrail.dto.RouteDTO;
+import com.infyrail.dto.TrainDTO;
 import com.infyrail.service.RouteService;
 
 @RestController
@@ -40,6 +44,12 @@ public class RouteController {
 		Integer id = routeService.createRoute(routeDTO);
 		String message = env.getProperty("route.create.success")+id.toString();
 		return new ResponseEntity<String>(message,HttpStatus.CREATED);		
+	}
+	
+	@GetMapping(value = "trains")
+	public ResponseEntity<List<TrainDTO>> getTrains(@RequestParam String source, @RequestParam String destination) throws NoSuchRouteException{
+		List<TrainDTO> trains = routeService.getTrains(source, destination);
+		return new ResponseEntity<List<TrainDTO>>(trains,HttpStatus.OK);
 	}
 
 }
