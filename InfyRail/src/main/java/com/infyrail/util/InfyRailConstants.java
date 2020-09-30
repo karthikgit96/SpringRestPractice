3f637cd4d7f54f118276770202b9afe0c4f20444
User <User@DESKTOP-C13PCB4>
2020-09-30 12:30:10 +0530

    Updation messages added

--- InfyRail/src/main/java/com/infyrail/util/InfyRailConstants.java
+++ InfyRail/src/main/java/com/infyrail/util/InfyRailConstants.java
@@ -3,6 +3,7 @@ package com.infyrail.util;
 public enum InfyRailConstants {
 	ROUTE_NOT_FOUND("route.not.found"),
 	ROUTE_ALREADY_PRESENT("route.already.present"),
+	ROUTE_UPDATE_SUCCESS("route.update.success"),
 	GENERAL_EXCEPTION_MESSAGE("general.exception");
 	
 	private final String type;
