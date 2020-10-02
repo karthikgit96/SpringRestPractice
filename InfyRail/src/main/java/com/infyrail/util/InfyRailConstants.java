21a13641d40b867c21dd61523728c79731d0914a
User <User@DESKTOP-C13PCB4>
2020-10-02 13:15:37 +0530

    Validation,Exception and success messages added

--- InfyRail/src/main/java/com/infyrail/util/InfyRailConstants.java
+++ InfyRail/src/main/java/com/infyrail/util/InfyRailConstants.java
@@ -4,6 +4,9 @@ public enum InfyRailConstants {
 	ROUTE_NOT_FOUND("route.not.found"),
 	ROUTE_ALREADY_PRESENT("route.already.present"),
 	ROUTE_UPDATE_SUCCESS("route.update.success"),
+	TRAIN_ALREADY_EXISTS("train.already.present"),
+	TRAIN_FARE_UPDATE_SUCCESS("train.fare.update.success"),
+	TRAIN_NOT_FOUND("train.not.found"),
 	GENERAL_EXCEPTION_MESSAGE("general.exception");
 	
 	private final String type;
