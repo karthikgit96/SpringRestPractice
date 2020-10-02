814f07053077fb45c1f2eba5092dba6d6255938c
User <User@DESKTOP-C13PCB4>
2020-10-02 13:12:50 +0530

    Custom exception class created for already existing trains

---
+++ InfyRail/src/main/java/com/infyrail/dto/TrainAlreadyExistsException.java
@@ -0,0 +1,14 @@
+package com.infyrail.dto;
+
+public class TrainAlreadyExistsException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public TrainAlreadyExistsException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
